9dc7164aa9f6bd4764d963b2c78837006eecad55
Apprentice <apprentice@dbc21.local>
2013-08-13 17:33:07 -0700

    create customized routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 SweetArticles::Application.routes.draw do
-  resources :articles
+  # /articles/:month/:day/:year/:title
+  match '/articles/:month/:day/:year/:title', to: 'articles#show', via: [:get]
+  # /categories/:category_name/articles/:title
+  match '/categories/:category_name/articles/:title', to: 'articles#show', via: [:get]
+  # /categories/:name/articles
+  match '/categories/:name/articles', to: 'articles#show', via: [:get]
+  # /tag/:name/articles
+  match '/tag/:name/articles', to: 'articles#show', via: [:get]
+
+  resources :articles, only: [:new, :create]
 end

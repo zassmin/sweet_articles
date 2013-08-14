d28bb518d8fe2b2936c9bc49a5516cd4d09eb3e8
Apprentice <apprentice@dbc21.local>
2013-08-13 17:39:54 -0700

    add a basic functionality to controller methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,21 @@
 class ArticlesController < ApplicationController
 
   def show
+    @articles = Article.all.group_by &:category
+    @article = Article.find_by_url "articles/#{params[:month]}/#{params[:day]}/#{params[:year]}/#{params[:title]}"
   end
 
   def new
+    @article = Article.new
   end
 
   def create
+    @article = Article.new params[:article]
+    if @article.save
+      redirect_to @article
+    else
+      p "did not save"
+    end
   end
 
 end
